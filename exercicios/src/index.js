4f157dcc130deef65298f8dfd790a992534620b5
diogo <diogo.lima.flooxai@gmail.com>
2025-05-12 10:39:39 -0300

    feat: add first component

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
+import Primeiro from './componentes/Primeiro';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Ola</h1>);
+root.render(<Primeiro/>);
